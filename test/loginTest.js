71ac6f067efef87ce728b981cadb3e3a892640ea
You Tzeng <joe5343281@gmail.com>
2017-09-22 12:08:18 +0800

    Use Chai replace assert of js and .gitignore

--- test/loginTest.js
+++ test/loginTest.js
@@ -1,22 +1,23 @@
 /*eslint-disable no-undef: "error"*/
 
-var assert = require('assert')
+//var assert = require('assert')
+var assert = require('chai').assert 
 
 describe('otrak', function() 
 {
     var M = require('./manager.js')
     
     describe('login', function() {
-        it('login(ccc, 1111))===undefined', function() {
-            assert.equal('undefined', typeof(M.log('ccc','1111')))
-        })
-            
+        it('login(ccc, 1111).isUndefined', function () {
+            assert.isUndefined(M.login('ccc', '1111'))
+        })   
+
         it('login(ccc, 1234567).id=ccc', function() {
             assert.equal('ccc', M.login('ccc', '1234567').id)
         })
         
         it('login(neo, 1111)===undefined', function() {
-            assert.equal('undefined', typeof(M.login('neo', '1111')))
+            assert.isUndefined(M.login('neo', '1111'))
         })
             
     })
